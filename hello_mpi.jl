5f38ab3e0c93f9c4b56ac0ccabfed78da5232805
Qihua Han <qihua.han@icloud.com>
2023-09-07 20:51:53 +0200

    Add MPI and package manager

--- hello_mpi.jl
+++ hello_mpi.jl
@@ -1,7 +1,9 @@
-# file hello_mpi.jl
 using MPI
 MPI.Init()
 comm = MPI.COMM_WORLD
 rank = MPI.Comm_rank(comm)
 nranks = MPI.Comm_size(comm)
 println("Hello world, I am rank $rank of $nranks")
+
+# julia> using MPI
+# julia> mpiexec(cmd->run(`$cmd -np 4 julia hello_mpi.jl`))
